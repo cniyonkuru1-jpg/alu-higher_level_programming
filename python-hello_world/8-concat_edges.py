384fdef6ecef4aa627382ec20602f7c57d6e2ef6
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-26 09:12:45 -0700

    add 8-concat_edges.py changes

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
-#!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-
-print(str.split()[5] + chr(32) + str.split()[6] + chr(32) + str.split()[12] + chr(32) + str.split()[0])
+str = "Python"
+str = str[3:]
+str += "object-oriented"
+str = str + " programming"
+print(str + " with " + "Python")
