020b369b8182001c3beb1ac8daba6f95f4f5007c
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-26 08:54:51 -0700

    add the changes made on 8-concatat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
- language that combines remarkable power with very clear syntax"
-print(str[39:67] + "with " + str[0:6])
+str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
+
+print(str.split()[5] + chr(32) + str.split()[6] + chr(32) + str.split()[12] + chr(32) + str.split()[0])
