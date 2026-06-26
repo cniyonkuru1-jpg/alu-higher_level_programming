2d0800abbc4a4f8a492a95de3e84d43f75594bcb
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-26 11:28:00 -0700

    Fix all files to not execute when imported

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
-args = sys.argv[1:]
-count = len(args)
-if count == 0:
-    print("0 arguments.")
-elif count == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(count))
-for i, arg in enumerate(args):
-    print("{}: {}".format(i + 1, arg))
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    count = len(args)
+    if count == 0:
+        print("0 arguments.")
+    elif count == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count))
+    for i, arg in enumerate(args):
+        print("{}: {}".format(i + 1, arg))
