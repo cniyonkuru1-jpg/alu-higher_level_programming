a94ac24610b9b6d9dea2f688856155eee60a7aed
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-26 10:42:52 -0700

    add changes made to 8-uppercase.py

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if ord('a') <= ord(c) <= ord('z'):
+        if 97 <= ord(c) <= 122:
             print("{}".format(chr(ord(c) - 32)), end="")
         else:
             print("{}".format(c), end="")
