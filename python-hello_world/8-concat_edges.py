ad3d5e68a0d1cd24db4224e71a4447ec2f9b8848
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-06-26 11:34:41 -0700

    Add 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming language with Python"
-print(str[39:52] + str[52:54] + str[54:66] + str[66:67] + str[67:70] + str[70:77] + str[77:78] + str[78:86])
+str = "Python is an interpreted, interactive, object-oriented programming\
+language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[105:111] + str[0:6])
