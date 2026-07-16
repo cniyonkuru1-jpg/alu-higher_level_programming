829a964d1788f72eb40bea1e801dff3d51b7a688
cniyonkuru1-jpg <c.niyonkuru1@alustudent.com>
2026-07-16 01:42:51 -0700

    add the changes

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-
 def safe_print_integer(value):
     """Print an integer with "{:d}".format().
 
@@ -8,12 +6,11 @@ def safe_print_integer(value):
         value (int): The integer to print.
 
     Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
+        True if the integer was printed successfully.
+        False if a TypeError or ValueError occurs.
     """
     try:
         print("{:d}".format(value))
-        return (True)
+        return True
     except (TypeError, ValueError):
-        return (False)
-
+        return False
