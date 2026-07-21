733160828c331a406034b60f525406b0f5826b0c
cniyonkuru1-jpg <cniyonkuru1-jpg@users.noreply.github.com>
2026-07-21 09:34:15 +0000

    Add module and class docstrings to 1-rectangle.py

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -1,11 +1,23 @@
 #!/usr/bin/python3
+"""Defines a Rectangle class with private width and height attributes."""
+
+
 class Rectangle:
+    """Represent a rectangle."""
+
     def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
         self.height = height
         self.width = width
 
     @property
     def width(self):
+        """Get/set the width of the rectangle."""
         return self.__width
 
     @width.setter
@@ -18,6 +30,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """Get/set the height of the rectangle."""
         return self.__height
 
     @height.setter
