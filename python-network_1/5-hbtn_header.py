41b95867dc9b93bc158eece41a76f471ef764f40
Chris <c.niyonkuru1@alustudent.com>
2026-07-30 21:44:56 +0200

    Refactor header display for clarity
    Updated docstring and simplified request handling.

--- python-network_1/5-hbtn_header.py
+++ python-network_1/5-hbtn_header.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Displays the value of the X-Request-Id header found in the
-response of a request sent to a given URL, using requests.
-"""
+"""Displays the X-Request-Id header value from a response."""
 import sys
 import requests
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    response = requests.get(url)
+    response = requests.get(sys.argv[1])
     print(response.headers.get("X-Request-Id"))
