71c32517634ff380f11f2acd2f746153c172ffc8
Chris <c.niyonkuru1@alustudent.com>
2026-07-30 21:47:08 +0200

    Refactor JSON API request and response handling

--- python-network_1/8-json_api.py
+++ python-network_1/8-json_api.py
@@ -1,24 +1,20 @@
 #!/usr/bin/python3
-"""Sends a POST request to the search_user endpoint with a letter
-parameter and displays the JSON result, using requests.
-"""
+"""Sends a POST request with a letter param and displays JSON results."""
 import sys
 import requests
 
 
 if __name__ == "__main__":
     letter = sys.argv[1] if len(sys.argv) > 1 else ""
-    response = requests.post(
-        "http://0.0.0.0:5000/search_user", data={"q": letter}
-    )
+    url = "http://0.0.0.0:5000/search_user"
+    response = requests.post(url, data={"q": letter})
+
     try:
-        json_data = response.json()
+        data = response.json()
     except ValueError:
         print("Not a valid JSON")
     else:
-        if not json_data:
+        if not data:
             print("No result")
         else:
-            print("[{}] {}".format(
-                json_data.get("id"), json_data.get("name")
-            ))
+            print("[{}] {}".format(data.get("id"), data.get("name")))
