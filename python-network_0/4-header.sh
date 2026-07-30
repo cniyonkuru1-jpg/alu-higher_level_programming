1a5a520af045d9254029f0b8fe267722609d852d
Chris <c.niyonkuru1@alustudent.com>
2026-07-30 21:01:21 +0200

    Modify header.sh to use GET request with query params
    Updated the script to use GET method with query parameters.

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the given URL with a custom header
-# X-HolbertonSchool-User-Id set to 98, and displays the body of the
-# response.
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# display the body of a url call 
+curl -sG "$1" -H "X-School-User-Id: 98"
