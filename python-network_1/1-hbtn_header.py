db063382000387867afb8995bf6d5073aa87a220
Chris <c.niyonkuru1@alustudent.com>
2026-07-30 21:09:02 +0200

    Refactor header retrieval in 1-hbtn_header.py
    Updated import statement and simplified URL handling.

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
-"""Displays the value of the X-Request-Id header found in the
-response of a request sent to a given URL, using urllib.
-"""
+"""Displays the value of the X-Request-Id header from a response."""
 import sys
-import urllib.request
+from urllib import request
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
-    with urllib.request.urlopen(url) as response:
+    with request.urlopen(sys.argv[1]) as response:
         print(response.getheader("X-Request-Id"))
