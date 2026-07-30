fdc3fae3d0880fac617f35f30b6efda843fae3bf
Chris <c.niyonkuru1@alustudent.com>
2026-07-30 20:58:03 +0200

    Change GET request to DELETE request in script

--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request (following redirects) and displays the body only if final status is 200
-curl -sL -w "%{http_code}" -o /tmp/body_$$ "$1" | grep -q "^200$" && cat /tmp/body_$$; rm -f /tmp/body_$$
+# Sends a DELETE request and displays the body of the response
+curl -s -X DELETE "$1"
