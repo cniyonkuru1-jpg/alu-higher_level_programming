130eef8afcb24228b89f4cc499f90cc4ee1998f1
Chris <c.niyonkuru1@alustudent.com>
2026-07-30 21:02:07 +0200

    Simplify POST request script
    Removed unnecessary comments and simplified the curl command.

--- python-network_0/5-post_params.sh
+++ python-network_0/5-post_params.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-# Sends a POST request to the given URL with email and subject
-# parameters, and displays the body of the response.
-curl -s -X POST "$1" \
-    -d "email=test@gmail.com" \
-    -d "subject=I will always be here for PLD"
+# Sends a POST request with email and subject params, displays the body
+curl -s -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
